deeaefda5a6c5cca6621a6d6231123bca5c45acc
Saverio Miroddi <saverio.pub2@gmail.com>
2022-09-25 23:47:44 +0200

    TEST

--- libclamav_rust/src/saverio.rs
+++ libclamav_rust/src/saverio.rs
@@ -2,3 +2,16 @@
 pub extern "C" fn ohyeah() {
     //
 }
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct message {}
+
+#[allow(non_camel_case_types)]
+type mime_type = libc::c_uint;
+
+#[allow(dead_code, non_snake_case)]
+pub(crate) fn messageGetMimeType(_m: *const message) -> mime_type {
+    // m.mimeType
+    todo!()
+}
