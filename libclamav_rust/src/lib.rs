2f3776a735de7f5f660f4693ee7f3b6dbb5f7c52
Saverio Miroddi <saverio.pub2@gmail.com>
2022-10-04 17:43:58 +0200

    Integrate transpiled project into main one (WIP: clean duplicate API, export headers?)

--- libclamav_rust/src/lib.rs
+++ libclamav_rust/src/lib.rs
@@ -1,4 +1,12 @@
 #![allow(clippy::all)]
+// Transpiled modules
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_assignments)]
+#![feature(extern_types)]
+#![feature(linkage)]
+#![allow(clashing_extern_declarations)]
 
 /*
  *  libclamav features written in Rust
@@ -30,3 +38,12 @@ pub mod ffi_util;
 pub mod fuzzy_hash;
 pub mod logging;
 pub mod util;
+
+// Transpiled modules
+
+#[macro_use]
+extern crate c2rust_bitfields;
+extern crate libc;
+
+pub mod mbox;
+pub mod message;
